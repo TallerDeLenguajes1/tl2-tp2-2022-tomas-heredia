dae3ae98a62720cb776a32ce6add316fc0ffea04
Soria Alvaro <81036909+mariafvilera@users.noreply.github.com>
2022-09-09 19:00:52 -0300

    comienzo

---
+++ LogTest/Program.cs
@@ -0,0 +1,27 @@
+﻿public static class Program
+{
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+    public static void Main()
+    {
+        try
+        {
+           Logger.Info("Hello world");
+           System.Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+           Logger.Error(ex, "Goodbye cruel world");
+        }
+
+        try
+        {
+           Logger.Debug("hola mundo en spanish");
+           System.Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+           Logger.Error(ex, "Goodbye cruel world");
+        }
+    }
+}  
